f652bfff7d28d43baae76ac54550e4bf67ccaede
Yumi1298 <nhb12980712@gmail.com>
2024-09-10 17:05:29 +0800

    app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+let nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY == 0) {
+    nav.style.boxShadow = "";
+  } else {
+    nav.style.boxShadow = "  0 5px 10px #a6a6a6";
+  }
+});
